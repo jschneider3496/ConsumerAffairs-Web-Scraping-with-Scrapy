52f80e5a05b28cf349e95861dfc7266ede29d3d1
Johannes Schneider <jojo20872@gmail.com>
2019-11-09 16:19:31 -0500

    Add comments to consumeraffairs-scrapy.py

--- consumeraffairs/consumeraffairs/spiders/consumeraffairs-scrapy.py
+++ consumeraffairs/consumeraffairs/spiders/consumeraffairs-scrapy.py
@@ -1,41 +1,38 @@
 import scrapy
 from ..items import ConsumeraffairsItem
 
-
+# Program web-scrapes date, comment, and star rating from all Uber reviews on https://www.consumeraffairs.com/
 class UberSpider(scrapy.Spider):
+    # Name of spider
     name = "uber_ca"
 
+    # Gather requests for spider
     def start_requests(self):
         urls = []
-        for x in range(0, 58):
+        # Append all pages from https://www.consumeraffairs.com/ to urls
+        for x in range(1, 58):
             url = 'https://www.consumeraffairs.com/travel/uber.html?page=' + str(x) + '#sort=recent&filter=none'
             urls.append(url)
-        print(urls)
+
+        # Scrape each page
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
+    # Web-scrape information
     def parse(self, response):
+        # Item object, acts like a simple container for information
         items = ConsumeraffairsItem()
 
-        # whole_reviews = response.xpath('//div[@class= "rvw js-rvw"]')
-
+        # Web-scrape date, stars, and comment
         date = response.xpath('//div[@class= "rvw js-rvw"]/div[3]/span/text()').extract()
         stars = response.xpath('//div[@class= "rvw js-rvw"]/div[1]/div/img/@data-rating').extract()
         comment = response.xpath('//div[@class= "rvw js-rvw"]/div[3]/p[2]/text()').extract()
         result = zip(date, stars, comment)
+
+        # Store date, stars, and comment to items
         for date, stars, comment in result:
             items['date'] = date
             items['stars'] = stars
             items['comment'] = comment
             yield items
 
-            # with open('reviews.txt', 'w') as f:
-            #     for u in comment:
-            #         f.write("Comment: " + u + "\n")
-            #     for u in stars:
-            #         f.write("Stars: " + u + "\n")
-            #     for u in date:
-            #         f.write("Date: " + u + "\n")
-
-
-# items = dict()
