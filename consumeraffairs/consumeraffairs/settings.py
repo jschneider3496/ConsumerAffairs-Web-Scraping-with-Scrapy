a7ca42bff4ce005295c8c4ef2970410a423ad58d
Johannes Schneider <jojo20872@gmail.com>
2019-11-09 16:56:37 -0500

    Reduce concurrent requests to 16

--- consumeraffairs/consumeraffairs/settings.py
+++ consumeraffairs/consumeraffairs/settings.py
@@ -22,7 +22,7 @@ NEWSPIDER_MODULE = 'consumeraffairs.spiders'
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+#CONCURRENT_REQUESTS = 16
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
